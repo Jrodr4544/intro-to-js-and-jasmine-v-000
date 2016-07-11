7e53e28b73329396109a025b51ea0b7411716341
Jrodr4544 <jelson.rodriguez@gmail.com>
2016-07-11 12:18:11 -0400

    Done.

--- code.js
+++ code.js
@@ -1,1 +1,20 @@
 'use strict';
+
+function favIceCream(iceCream) {
+  var phrase = `I love ${iceCream}`;
+  return phrase;
+}
+
+function shouting() {
+  var phrase = "i love javascript soooooo much";
+  return phrase.toUpperCase();
+}
+
+function roundDown(number) {
+  var result = Math.floor(number);
+  return result;
+}
+
+function theTruth() {
+  return true;
+}
